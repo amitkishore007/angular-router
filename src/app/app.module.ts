68267776aaba2341769c80512b57447997064409
Amit Kishore <amit.kishore@biz2credit.org>
2018-09-18 16:15:46 +0530

    router layout and config and layout setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MyserverComponent } from './myserver/myserver.component';
+import { UsersComponent } from './users/users.component';
+
+const appRoutes: Routes = [
+  {path: '/',component:HomeComponent},
+  {path:'users',component:UsersComponent},
+  {path:'myserver',component:MyserverComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MyserverComponent,
+    UsersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
